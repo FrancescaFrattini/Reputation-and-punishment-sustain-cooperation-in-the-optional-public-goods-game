f84d372f301a4a879a5c89845688c5862139f840
FrancescaFrattini <f.frattini@unimore.it>
2025-07-22 20:42:06 +0200

    style: added documentation for implemented methods

--- opgar/agent.py
+++ opgar/agent.py
@@ -17,9 +17,10 @@ class _Agent:
         self.utility = 1
         self.reputation = 1
         self.tracker = None
+        """
         logging.info(
             f"Agent {self.ID} created with r={self.reputation} & s={self.strategy}"
-        )
+        ) """
 
     def _choose_action(self, average_reputation):
         """
@@ -57,7 +58,6 @@ class _Agent:
         )
     
 class QLearningAgent(_Agent):
-# uses Strategy XII - Q-Learning without states (only 3 actions)
 
     __slots__ = _Agent.__slots__ + [
     "q_values", "alpha", "discount_factor", "epsilon"
@@ -67,24 +67,53 @@ class QLearningAgent(_Agent):
 
     ACTIONS = [1, 0, None]  # Actions: cooperate (1), defect (0), withdraw (None)
 
-    def __init__(self, ID, alpha=0.1, discount_factor=0.2, epsilon=1.0):
-        # NB: strategy root “XII”, no punischment component
-        super().__init__(ID, strategy="XII_NNN")
+    def __init__(self, ID, strategy, alpha=0.1, discount_factor=0.1, epsilon=1.0):
+        super().__init__(ID, strategy=strategy)
         self.alpha, self.discount_factor, self.epsilon = alpha, discount_factor, epsilon
         self.q_values = np.zeros(len(self.ACTIONS))
 
     def _choose_action(self, average_reputation):
-        # ignoring average_reputation for now, as this is a Q-Learning agent
-        #epsilon decay
+        """
+        For Q-Learning agents there are two options for action selection:
+        1. Epsilon-greedy action selection: with probability epsilon, choose a random action
+            Epsilon value decays at every time step of a factor of 0.99, ensuring exploration
+        2. Greedy action selection: choose the action with the highest Q-value
+
+        Args:
+            average_reputation (float): The average reputation in [-1, 1] of the other players in the group
+            ignoring average_reputation for now, as this is a Q-Learning agent
+        
+        Returns:
+            Action (str): Contributes 1 or 0 if playing, if not participating, then return None
+        """
+        logging.info(
+            f"avg reputation is {average_reputation}"
+            ) 
         if self.epsilon > self.minimum_epsilon:
             self.epsilon *= 0.99
-        if random.random() < self.epsilon:
-            return random.choice(self.ACTIONS)
+            if random.random() < self.epsilon:
+                return random.choice(self.ACTIONS)
         return self.ACTIONS[int(np.argmax(self.q_values))]
 
     def learn(self, reward, action_taken):
+        """
+        Updates the agent's Q-values based on the action taken and the received reward.
+            The Q-value for the action taken is updated using the Q-learning formula:
+            Q(a) = Q(a) + α * (reward + γ * max(Q(a')) - Q(a))
+            where:
+            - Q(a) is the current Q-value for the action taken  
+            - α is the learning rate
+            - reward is the immediate reward received for the action taken
+            - γ is the discount factor
+            - max(Q(a')) is the maximum Q-value for the available actions, as there are no states here   
+        Args:
+            reward (float): The reward received for the action taken
+            action_taken (str): The action taken by the agent, one of [1, 0, None]
+
+        Returns: 
+            None      
+        """
         idx = self.ACTIONS.index(action_taken)
-        #as there are no states, we multiply the discount factor by the max Q-value
-        td_error = reward  + (self.discount_factor*np.max(self.q_values)) - self.q_values[idx]      
+        td_error = reward  + (self.discount_factor*np.max(self.q_values)) - self.q_values[idx]    
         self.q_values[idx] += self.alpha * td_error
         
