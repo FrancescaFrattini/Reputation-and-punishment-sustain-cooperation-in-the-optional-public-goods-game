46e1c9742ef7e8c2d2d364aec7ce789c6bc64407
FrancescaFrattini <f.frattini@unimore.it>
2025-11-07 14:16:25 +0100

    feat: added unconditional loner + q-learner strategy

--- opgar/strategy.py
+++ opgar/strategy.py
@@ -132,6 +132,7 @@ class _Strategy:
         "Q-Learning": all_strategies[-8:],
         "UnconditionalCooperator + Q-Learning": ["I_NNN", "XII_NNN"],
         "UnconditionalDefector + Q-Learning": ["II_NNN", "XII_NNN"],
+        "UnconditionalLoner + Q-Learning": ["III_NNN", "XII_NNN"],
         "Q-Learning no punishment": ["XII_NNN"]
     }
 
