1ab1e6e60c48a70a3ed6fcc42ee8cd6de03ac1e0
FrancescaFrattini <f.frattini@unimore.it>
2026-07-07 21:47:05 +0200

    build: updated setup script

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
    name='opgar',
-   version='1.0',
-   description='Optional Public Good game And Reputation',
+   version='2.0',
+   description='Optional Public Good game And Reputation with Q-Learning',
    author='Shirsendu Podder',
    author_email='ucabpod@ucl.ac.uk',
+   maintainer='Francesca Frattini',
+   maintainer_email='f.frattini@unimore.it',
    packages=['opgar'],  #same as name
    install_requires=['numpy', 'pandas', 'tqdm', 'networkx'], #external packages as dependencies
 )
